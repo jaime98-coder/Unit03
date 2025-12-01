a09cbf565f8871d11c5d01be89ae2e330225d9ed
jaime98-coder <jaime.martin@iesnervion.es>
2025-12-01 11:56:28 +0100

    unit 03

---
+++ src/arraysexercises/Exerciose02.java
@@ -0,0 +1,21 @@
+package arraysexercises;
+
+import java.util.Scanner;
+
+public class Exerciose02 {
+
+	public static void main(String[] args) {
+		// Creo una tabla con 5 de longitud (indíces del 0 al 4)
+		double numeros[] = new double [5];
+		Scanner sc = new Scanner(System.in);
+		// Creo un bucle for para ir añadiendo valores a la tabla en las diferentes posiciones.
+		for (int i=0; i<numeros.length; i++) {
+			System.out.println("Introduce un valor "+i);
+			numeros=sc.nextDouble();
+		}
+
+		
+		sc.close();
+	}
+
+}
---
+++ src/arraysexercises/Exercise01.java
@@ -0,0 +1,17 @@
+package arraysexercises;
+
+import java.util.Random;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		int numeros [] = new int [10];
+		Random aleatorio = new Random();
+		for (int i=0; i<numeros.length; i++) {
+			numeros[i]=aleatorio.nextInt(1, 101);
+			System.out.println("Índice "+i+": "+numeros[i]);
+		}
+		
+	}
+
+}
