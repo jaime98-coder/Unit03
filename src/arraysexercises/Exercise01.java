7cb2fc696fd508531d552210c6c573f1096d3253
jaime98-coder <jaime.martin@iesnervion.es>
2025-12-04 14:40:52 +0100

    subida unit 3

--- src/arraysexercises/Exercise01.java
+++ src/arraysexercises/Exercise01.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Exercise01 {
 
 	public static void main(String[] args) {
-		int numeros [] = new int [10];
+		int [] numeros  = new int [10];
 		Random aleatorio = new Random();
 		for (int i=0; i<numeros.length; i++) {
 			numeros[i]=aleatorio.nextInt(1, 101);
--- src/arraysexercises/Exercise02.java
+++ src/arraysexercises/Exercise02.java
@@ -4,18 +4,36 @@ import java.util.Scanner;
 
 public class Exercise02 {
 
-	public static void main(String[] args) {
-		// Creo una tabla con 5 de longitud (indíces del 0 al 4)
-		double numeros[] = new double [5];
-		Scanner sc = new Scanner(System.in);
-		// Creo un bucle for para ir añadiendo valores a la tabla en las diferentes posiciones.
-		for (int i=0; i<numeros.length; i++) {
-			System.out.println("Introduce un valor "+i);
-			numeros=sc.nextDouble();
-		}
+    public static void main(String[] args) {
+        
+        // --- 1. Declaración e Inicialización ---
+        
+        // Es buena práctica en Java poner los corchetes junto al tipo de dato: double[]
+        double[] numeros = new double[5]; 
+        
+        Scanner sc = new Scanner(System.in);
 
-		
-		sc.close();
-	}
+        // --- 2. Entrada de datos ---
+        
+        // Bucle para rellenar el array
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Introduce el valor para la posición " + i + ":");
+            // Correcto: asignamos al índice 'i'
+            numeros[i] = sc.nextDouble(); 
+        }
+
+        // --- 3. Salida de datos ---
+
+        System.out.println("Los valores introducidos son:");
+        
+        // Necesitamos OTRO bucle para recorrer y mostrar el contenido
+        for (int i = 0; i < numeros.length; i++) {
+            // Aquí accedemos a la posición 'i' para mostrarla
+            System.out.println("En la posición " + i + " hay un: " + numeros[i]);
+        }
+        
+        // --- 4. Cierre ---
+        sc.close();
+    }
 
 }
