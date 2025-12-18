ee55c6d104e6c1391a66680439cb40f24db8db69
jaime98-coder <jaime.martin@iesnervion.es>
2025-12-18 10:13:55 +0100

    subida ejercicios 3 y 4

--- src/arraysexercises02/Exercise03.java
+++ src/arraysexercises02/Exercise03.java
@@ -1,23 +1,30 @@
 package arraysexercises02;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Exercise03 {
-
+	// Crea un programa que cree un array de tamaño 30 y lo rellene con
+	// valores aleatorios entre 0 y 9. Luego ordena los valores del array y los
+	// mostrará por pantalla.
 	public static void main(String[] args) {
-		int [] array = new int [30];
-		int valoresAleatorios;
+
+		int[] numeros = new int[30];
 		Random aleatorio = new Random();
-		for (int i=0; i<array.length; i++) {
-			array[i] = aleatorio.nextInt(10);
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = aleatorio.nextInt(10);
 		}
 		System.out.println("Contenido del array:");
-		for (int i=0; i<array.length; i++) {
-			System.out.print(array[i] + " ");
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
 		}
+		System.out.println();
+
 		System.out.println("Contenido del array ordenado:");
-		for (int i=0; i<array.length; i++) {
-			System.out.print(Arrays.sort[i] + " ");
+		Arrays.sort(numeros);
+		for (int i = 0; i < numeros.length; i++) {
+
+			System.out.print(numeros[i] + " ");
 		}
 
 	}
---
+++ src/arraysexercises02/Exercise04.java
@@ -0,0 +1,35 @@
+package arraysexercises02;
+
+import java.util.Scanner;
+
+public class Exercise04 {
+	// Necesitamos crear un programa para mostrar el ranking de puntuaciones
+	// de un torneo de ajedrez con 8 jugadores. Se le pedirá al usuario que
+	// introduzca las puntuaciones de todos los jugadores (habitualmente valores
+	// entre 1000 y 2800, de tipo entero) y luego muestre las puntuaciones en orden
+	// descendente (de la más alta a la más baja).
+	public static void main(String[] args) {
+		System.out.println("Sistema de Puntuaciones del torneo de ajedrez:");
+		int[] puntuaciones = new int[8];
+		//variable auxiliar
+		int[]puntuacionesPorJugador = new int;
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < puntuaciones.length; i++) {
+			puntuaciones[i] = sc.nextInt();
+			while (puntuaciones[i] < 1000 || puntuaciones[i] > 2800) {
+				System.out.println("Por favor, introduce una puntuación válida");
+				puntuaciones[i] = sc.nextInt();
+			}
+			puntuacionesPorJugador
+			
+			System.out.println("Puntos jugador " + (i + 1) + ": " + puntuaciones[i]);
+		}
+		System.out.println("Tabla de puntos");
+		for (int i=0; i<puntuaciones.length;i++) {
+			System.out.println(puntuaciones[i]);
+		}
+
+		sc.close();
+	}
+
+}
